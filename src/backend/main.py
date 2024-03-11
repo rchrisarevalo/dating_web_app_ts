c90d947c61ff167e1ebf8afb1bd0b4cfa06b21c0
rchrisarevalo <rubenplayer105@gmail.com>
2024-03-10 23:42:37 -0500

    Added types for functions for better readability and functionality

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,24 +1,24 @@
 from similarity_calculations import run_algo
 import time
 
-def run_matching_algorithm(user_profiles, logged_in_user_profile, use_so_filter):
+def run_matching_algorithm(user_profiles: list[dict[str, any]], logged_in_user_profile: list[dict[str, any]], use_so_filter: bool):
     matched_users = run_algo(profile_records=user_profiles, logged_in_user_record=logged_in_user_profile, use_so_filter=use_so_filter)
     return matched_users
 
 
-def return_run_time(user_profiles, logged_in_user_profile):
+def return_run_time(user_profiles: list[dict[str, any]], logged_in_user_profile: list[dict[str, any]], use_so_filter: bool):
     start = time.time()
-    run_algo(profile_records=user_profiles, logged_in_user_record=logged_in_user_profile)
+    run_algo(profile_records=user_profiles, logged_in_user_record=logged_in_user_profile, use_so_filter=use_so_filter)
     end = time.time()
     overall = end - start
     return overall
 
 
-def return_run_time_multiple(user_profiles, logged_in_user_profile):
+def return_run_time_multiple(user_profiles: list[dict[str, any]], logged_in_user_profile: list[dict[str, any]], use_so_filter: bool):
     start = time.time()
     
     for i in range(0, 10000):
-        run_algo(profile_records=user_profiles, logged_in_user_record=logged_in_user_profile)
+        run_algo(profile_records=user_profiles, logged_in_user_record=logged_in_user_profile, use_so_filter=use_so_filter)
     
     end = time.time()
     overall = end - start
