82d36362d9213bfa3b20add17c92d0c37b83dde1
rchrisarevalo <rubenplayer105@gmail.com>
2024-05-25 02:21:23 -0500

    Randomized matches

--- run.py
+++ run.py
@@ -10,9 +10,6 @@ import time
 
 sys.path.append('./src/backend')
 
-def run_environ(cmd: str):
-    subprocess.run(cmd, shell=True, check=True)
-
 def run_express_server(cmd: str):
     subprocess.run(cmd, shell=True, check=True, cwd="./src/backend")
 
@@ -20,18 +17,15 @@ def run_python_server(cmd: str):
     subprocess.run(cmd, shell=True, check=True, cwd="./src/backend")
 
 if __name__ == "__main__":
-    dev_t = t.Thread(target=run_environ, args=("npm run dev",))
     js_t = t.Thread(target=run_express_server, args=("nodemon server.js",))
     f_api_t = t.Thread(target=run_python_server, args=("uvicorn server:server --reload --port 5000",))
 
     # Run the threads.
-    dev_t.start()
     js_t.start()
     f_api_t.start()
 
     # Join the threads so that they continue to run until
     # the user terminates the script.
-    dev_t.join()
     time.sleep(1.0)
     js_t.join()
     time.sleep(1.0)
