aa01adc0b774c5b545ac74a733b5673efda2cb98
rchrisarevalo <rubenplayer105@gmail.com>
2024-06-20 19:19:32 -0500

    UI for displaying matches updated

--- src/components/Wrappers.tsx
+++ src/components/Wrappers.tsx
@@ -21,3 +21,19 @@ export const CenterWrapper = (props: WrapperProps) => {
         </div>
     )
 }
+
+export const SideScrollWrapper: React.FC<WrapperProps> = ({ children }) => {
+    return (
+        <div className="side-scroll-wrapper">
+            { children }
+        </div>
+    )
+}
+
+export const SideScrollInnerWrapper: React.FC<WrapperProps> = ({ children }) => {
+    return (
+        <div className="side-scroll-inner-wrapper">
+            { children }
+        </div>
+    )
+}
--- src/page-components/SearchPage.tsx
+++ src/page-components/SearchPage.tsx
@@ -8,6 +8,7 @@ import { clearSearchTerm, insertSearchTerm } from '../functions/searchHistoryFun
 
 import Spinner from 'react-bootstrap/Spinner'
 import { CurrentUserProfileContext } from '../components/Contexts'
+import { SideScrollInnerWrapper, SideScrollWrapper } from '../components/Wrappers'
 
 export const SearchPage = () => {
     const profileContext = useContext(CurrentUserProfileContext)
@@ -58,87 +59,77 @@ export const SearchPage = () => {
 
     return (
         <div className="search-page-container">
-            <div className="search-page-row">
-                <h1>SEARCH FOR USER</h1>
-                <br></br>
-                {(!matched_profiles.pending && !pending && !algo_pending) ?
+            {(!matched_profiles.pending && !pending && !algo_pending) ?
+                (!matched_profiles.error && !error && !algo_error) ?
                     <>
-                        {(!matched_profiles.error && !error && !algo_error) ?
+                        <div className='search-page-row'>
+                            <h1>SEARCH FOR USER</h1>
+                            <br></br>
+                            <form className="search-page-col" onSubmit={(e) => e.preventDefault()}>
+                                <input id="search-input" type="search" placeholder="Enter a search term..." value={currentSearchTerm} onChange={(e) => setCurrentSearchTerm(e.target.value)} autoComplete='off' required />
+                                <IoFunnelOutline size={30} style={{ marginLeft: '10px', marginRight: '10px', cursor: 'pointer' }} />
+                                <IoHelpOutline size={30} style={{ marginLeft: '10px', marginRight: '10px', cursor: 'pointer' }} title={algo_config ? 'You will receive matched users in your results.' : 'You will receive regular search results.'} />
+                            </form>
+                            <br></br>
                             <>
-                                <form className="search-page-col" onSubmit={(e) => e.preventDefault()}>
-                                    <input id="search-input" type="search" placeholder="Enter a search term..." value={currentSearchTerm} onChange={(e) => setCurrentSearchTerm(e.target.value)} autoComplete='off' required />
-                                    <IoFunnelOutline size={30} style={{ marginLeft: '10px', marginRight: '10px', cursor: 'pointer' }} />
-                                    <IoHelpOutline size={30} style={{ marginLeft: '10px', marginRight: '10px', cursor: 'pointer' }} title={algo_config ? 'You will receive matched users in your results.' : 'You will receive regular search results.'} />
-                                </form>
-                                <br></br>
-                                <>
-                                    {searchHistory.length !== 0 &&
-                                        <>
-                                            {searchHistory.map((term, i) =>
-                                                <div id="search-history-link" key={`search-history-term-${i}`}>
-                                                    {typeof (term.search_term) !== "undefined" &&
-                                                        <>
-                                                            <div id="search-history-term">
-                                                                <input id={`search-term-${i}`} value={`${term.search_term}`} disabled></input>
-                                                            </div>
-                                                            <div id="search-history-options">
-                                                                <button><IoSearchOutline size={30} style={{ cursor: 'pointer' }} onClick={() => setCurrentSearchTerm(term.search_term)} /></button>
-                                                                <button onClick={() => clearSearchTerm(i, setSearchHistory)}><IoTrashBinOutline size={30} style={{ cursor: 'pointer' }} /></button>
-                                                            </div>
-                                                        </>
-                                                    }
-                                                </div>
-                                            )}
-                                        </>
-                                    }
-                                    <br></br>
+                                {searchHistory.length !== 0 &&
                                     <>
-                                        {results.length !== 0 ?
-                                            <>
-                                                {results.map((result, i) =>
+                                        {searchHistory.map((term, i) =>
+                                            <div id="search-history-link" key={`search-history-term-${i}`}>
+                                                {typeof (term.search_term) !== "undefined" &&
                                                     <>
-                                                        <figure id="new-card" key={`profile-${i}`}>
-                                                            <div id="new-card-image">
-                                                                <img src={`data:image/png;base64,${result.uri}`} alt="search-profile-pic"></img>
-                                                            </div>
-                                                            <br></br>
-                                                            <div id="user-profile-figure-container" key={`user-profile-figure-${i}`}>
-                                                                <h3>{`${result.first_name}, ${result.age}`}</h3>
-                                                                <p id="user-profile-details">{`${result.city_residence}, ${result.state_residence}`}</p>
-                                                                {result.interests.split("\n").map((paragraph) =>
-                                                                    <p id="user-profile-details">
-                                                                        <i>{`${paragraph}`}</i>
-                                                                    </p>
-                                                                )}
-                                                                <Link to={`/user/${result.username}`} onClick={() => insertSearchTerm()}>View</Link>
-                                                            </div>
-                                                        </figure>
-                                                        <br></br>
-                                                        <br></br>
+                                                        <div id="search-history-term">
+                                                            <input id={`search-term-${i}`} value={`${term.search_term}`} disabled></input>
+                                                        </div>
+                                                        <div id="search-history-options">
+                                                            <button><IoSearchOutline size={30} style={{ cursor: 'pointer' }} onClick={() => setCurrentSearchTerm(term.search_term)} /></button>
+                                                            <button onClick={() => clearSearchTerm(i, setSearchHistory)}><IoTrashBinOutline size={30} style={{ cursor: 'pointer' }} /></button>
+                                                        </div>
                                                     </>
-                                                )}
-                                            </>
-                                            :
-                                            <h5 style={{ marginTop: '50px' }}>There are no search results to display.</h5>
-                                        }
+                                                }
+                                            </div>
+                                        )}
                                     </>
-                                </>
-                            </>
-                            :
-                            <div className="loading-match-page">
-                                <p>Failed to load or perform request. Please try again.</p>
+                                }
                                 <br></br>
-                                <button style={{background: 'white', border: 'none', padding: '7px 30px', borderRadius: '20px', textTransform: 'uppercase', fontWeight: '900'}} onClick={() => window.location.reload()}>Reload Page</button>
-                            </div>
-                        }
+                            </>
+                        </div>
+                        <SideScrollWrapper>
+                            <SideScrollInnerWrapper>
+                                {results.length !== 0 ?
+                                    results.map((result, i) =>
+                                        <figure id="profile-card" key={`profile-${i}`}>
+                                            <img src={`data:image/png;base64,${result.uri}`} alt="search-profile-pic"></img>
+                                            <br></br>
+                                            <div id="user-profile-figure-container">
+                                                <h3>{`${result.first_name}, ${result.age}`}</h3>
+                                                <p id="user-profile-details">{`${result.city_residence}, ${result.state_residence}`}</p>
+                                                <i id="user-profile-details">{`${result.interests.slice(0, 100)}...`}</i>
+                                                <br></br>
+                                                <br></br>
+                                                <br></br>
+                                                <Link to={`/user/${result.username}`} onClick={() => insertSearchTerm()}>View</Link>
+                                            </div>
+                                        </figure>
+                                    )
+                                    :
+                                    <h5 style={{ marginTop: '50px' }}>There are no search results to display.</h5>
+                                }
+                            </SideScrollInnerWrapper>
+                        </SideScrollWrapper>
                     </>
                     :
                     <div className="loading-match-page">
-                        <Spinner animation='border' />
-                        <p>Loading the best matches for you...</p>
+                        <p>Failed to load or perform request. Please try again.</p>
+                        <br></br>
+                        <button style={{ background: 'white', border: 'none', padding: '7px 30px', borderRadius: '20px', textTransform: 'uppercase', fontWeight: '900' }} onClick={() => window.location.reload()}>Reload Page</button>
                     </div>
-                }
-            </div>
+                :
+                <div className="loading-match-page">
+                    <Spinner animation='border' />
+                    <p>Loading the best matches for you...</p>
+                </div>
+            }
         </div>
     )
 }
